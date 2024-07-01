076259ac4138c75ffd35cfc8a60c18da2742b858
schilkp <schilk.philipp@gmail.com>
2024-07-01 12:06:45 +0200

    Web: Make whole upload div clickable.

--- web/website/src/components/DataCard/UploadArea.vue
+++ web/website/src/components/DataCard/UploadArea.vue
@@ -36,45 +36,55 @@ function delete_file() {
   selected_file.value = null;
 }
 
+function select_file_click() {
+  document.getElementById("upload_select_form")?.click();
+}
+
 defineExpose({ selected_file });
 </script>
 
+<!-- :style=" () => { if (selected_file !== null) { return ''; } else { return 'cursor: pointer;' ; } } " -->
+
 <template>
   <div
-    class="uk-placeholder uk-text-center uk-height-small"
+    class="uk-placeholder uk-text-center uk-height-small uk-padding-remove"
     @dragover="drag_over"
     @dragenter="drag_enter"
     @dragexit="drag_exit"
     @drop="drag_drop"
     :class="dragover_active"
   >
-    <div>
+    <div
+      v-if="selected_file === null"
+      class="uk-height-1-1 uk-padding"
+      @click="select_file_click"
+      style="cursor: pointer"
+    >
       <img
-        v-if="selected_file === null"
         src="../../../assets/upload.svg"
         alt="Upload"
         height="35"
         width="35"
       />
+      <p />
+      <span class="uk-text-middle"> Drop trace file or click to select</span>
+      <div uk-form-custom>
+        <input
+          type="file"
+          @change="upload_file_changed"
+          id="upload_select_form"
+          single
+        />
+      </div>
+    </div>
+    <div v-if="selected_file !== null" class="uk-height-1-1 uk-padding">
       <img
-        v-if="selected_file !== null"
         src="../../../assets/file.svg"
         alt="Uploaded File"
         height="35"
         width="35"
       />
-    </div>
-
-    <p />
-
-    <div v-if="selected_file === null">
-      <span class="uk-text-middle"> Drop trace file here or </span>
-      <div uk-form-custom>
-        <input type="file" @change="upload_file_changed" single />
-        <span class="uk-link">select it</span>.
-      </div>
-    </div>
-    <div v-if="selected_file !== null">
+      <p />
       {{ selected_file.name }}
       <button @click="delete_file" type="button" uk-close></button>
     </div>
