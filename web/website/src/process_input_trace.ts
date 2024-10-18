7a718ecd74b89c938526cbaee06017d2a69ab8c1
schilkp <schilk.philipp@gmail.com>
2024-10-18 09:12:47 +0200

    feat(web): Ignore buffer start/end markers for non-binary input.

--- web/website/src/process_input_trace.ts
+++ web/website/src/process_input_trace.ts
@@ -1,3 +1,4 @@
+import { ui_info } from "./log";
 import { TraceDataPiece } from "./state";
 
 
@@ -5,37 +6,61 @@ export function process_input_trace(content: string | ArrayBuffer, format: strin
     switch (format) {
         default:
         case "hex":
-            const inp = <string>content;
-            var hex = "";
-            for (var c of inp) {
-                if (/\s/.test(c)) {
-                    continue;
-                }
-                if (!/[a-fA-F0-9]/.test(c)) {
-                    throw "Non-hex char '" + c + "'.";
-                }
-                hex = hex + c;
-            }
-            if (hex.length === 0) {
-                throw "Empty";
-            }
-            if (hex.length % 2 !== 0) {
-                throw "Non-even number of hex characters.";
-            }
-
-            let match = hex.match(/.{1,2}/g);
-            if (!match) {
-                throw "Malformed.";
-            }
-            return new TraceDataPiece(core_id, Uint8Array.from(match.map((byte) => parseInt(byte, 16))));
+            return process_hex_trace(<string>content, core_id);
         case "base64":
-            var binaryString = atob(<string>content);
-            var bytes = new Uint8Array(binaryString.length);
-            for (var i = 0; i < binaryString.length; i++) {
-                bytes[i] = binaryString.charCodeAt(i);
-            }
-            return new TraceDataPiece(core_id, bytes);
+            return process_b64_trace(<string>content, core_id);
         case "binary":
             return new TraceDataPiece(core_id, new Uint8Array(<ArrayBuffer>content));
     }
+}
+
+
+function remove_markers(content: string): string {
+    const markers = ["==== START OF TONBANDGERAET BUFFER ====", "==== END OF TONBANDGERAET BUFFER ===="]
+
+    for (var marker of markers) {
+        if (content.includes(marker)) {
+            ui_info("Ignoring marker \'" + marker + "\'");
+        }
+        content = content.replace(marker, "");
+    }
+
+    return content
+}
+
+function process_hex_trace(content: string, core_id: number): TraceDataPiece {
+    let inp = remove_markers(<string>content);
+
+    var hex = "";
+
+    for (var c of inp) {
+        if (/\s/.test(c)) {
+            continue;
+        }
+        if (!/[a-fA-F0-9]/.test(c)) {
+            throw "Non-hex char '" + c + "'.";
+        }
+        hex = hex + c;
+    }
+    if (hex.length === 0) {
+        throw "Empty";
+    }
+    if (hex.length % 2 !== 0) {
+        throw "Non-even number of hex characters.";
+    }
+
+    let match = hex.match(/.{1,2}/g);
+    if (!match) {
+        throw "Malformed.";
+    }
+    return new TraceDataPiece(core_id, Uint8Array.from(match.map((byte) => parseInt(byte, 16))));
+}
+
+function process_b64_trace(content: string, core_id: number): TraceDataPiece {
+    var binaryString = atob(remove_markers(<string>content));
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return new TraceDataPiece(core_id, bytes);
 }
