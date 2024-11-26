8b781efaf835c265afa826508a5e4e8fe9be2069
schilkp <schilk.philipp@gmail.com>
2024-11-26 13:03:49 +0100

    fix(tband): For now, disable all interrupts in baremetal CM4F port.

--- tband/portable/GCC_ARM_CM4F/tband_port.h
+++ tband/portable/GCC_ARM_CM4F/tband_port.h
@@ -8,20 +8,15 @@
  */
 #ifndef TBAND_PORT_H_
 
-#ifndef tband_portMAX_INTERRUPT_PRIORITY
-#define tband_portMAX_INTERRUPT_PRIORITY (0x00)
-#endif
-
 #define tband_portENTER_CRITICAL_FROM_ANY()                                                        \
-  unsigned int tband_port_prev_basepri;                                                            \
-  __asm volatile("mrs   %0, basepri      \n\t" /* Save previous basepri value */                   \
-                 "mov   r1, %1           \n\t" /* Move new basepri value to r1 */                  \
-                 "msr   basepri_max, r1  \n\t" /* Apply new basepri value from r1 */               \
-                 : "=r"(tband_port_prev_basepri)                                                   \
-                 : "i"(tband_portMAX_INTERRUPT_PRIORITY)                                           \
+  unsigned int tband_port_prev_primask;                                                            \
+  __asm volatile("mrs   %0, primask  \n\t" /* Save previous primask value */                       \
+                 "cpsid i            \n\t" /* Disable interrupts */                                \
+                 : "=r"(tband_port_prev_primask)                                                   \
+                 :                                                                                 \
                  : "r1", "cc");
 
 #define tband_portEXIT_CRITICAL_FROM_ANY()                                                         \
-  __asm volatile("msr   basepri, %0  \n\t" : : "r"(tband_port_prev_basepri) :);
+  __asm volatile("msr   primask, %0  \n\t" : : "r"(tband_port_prev_primask) : "memory");
 
 #endif /* TBAND_PORT_H_ */
