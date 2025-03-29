15424d4a4da62f1584b83c7b1e13134738a32b8e
schilkp <schilk.philipp@gmail.com>
2025-03-29 13:18:12 +0100

    fix(web): Correct header card layout

--- web/website/src/components/HeaderCard/HeaderCard.vue
+++ web/website/src/components/HeaderCard/HeaderCard.vue
@@ -8,17 +8,19 @@ import ModeInput from "./ModeInput.vue";
     <div
         class="uk-card uk-card-default uk-card-body uk-margin-left uk-margin-right uk-margin-top uk-margin-bottom"
     >
-        <div class="uk-width-1-1@s">
-            <h1><strong>Tonbandgerät</strong></h1>
-        </div>
-        <div class="uk-width-1-3@s">
-            <ModeInput />
-        </div>
-        <div class="uk-width-1-3@s">
-            <CoreCountInput />
-        </div>
-        <div class="uk-width-1-3@s">
-            <DemoSelect />
+        <div class="uk-child-width-expands@s uk-grid-match" uk-grid>
+            <div class="uk-width-1-1@s">
+                <h1><strong>Tonbandgerät</strong></h1>
+            </div>
+            <div class="uk-width-1-3@s">
+                <ModeInput />
+            </div>
+            <div class="uk-width-1-3@s">
+                <CoreCountInput />
+            </div>
+            <div class="uk-width-1-3@s">
+                <DemoSelect />
+            </div>
         </div>
     </div>
 </template>
