2c32ccb15a9eac46145d5a6b821b870562571376
brendan <brendanawjang@gmail.com>
2026-08-28 11:53:34 +0800

    add version hooks for traceSTARTING_SCHEDULER

--- tband/src/tband_freertos.c
+++ tband/src/tband_freertos.c
@@ -32,6 +32,19 @@
 #error "configUSE_TRACE_FACILITY is not enabled!"
 #endif /* configUSE_TRACE_FACILITY == 0 */
 
+#ifndef tband_configFREERTOS_VERSION_MAJOR
+#define tband_configFREERTOS_VERSION_MAJOR tskKERNEL_VERSION_MAJOR
+#endif
+#ifndef tband_configFREERTOS_VERSION_MINOR
+#define tband_configFREERTOS_VERSION_MINOR tskKERNEL_VERSION_MINOR
+#endif
+#ifndef tband_configFREERTOS_VERSION_BUILD
+#define tband_configFREERTOS_VERSION_BUILD tskKERNEL_VERSION_BUILD
+#endif
+#if !TBAND_FREERTOS_HAVE_VERSION(10, 3, 1)
+#error "Minimum supported FreeRTOS version is 10.3.1"
+#endif
+
 // ===== STATE =================================================================
 
 // Unique ID counters (shared between all cores)
@@ -48,12 +61,40 @@ static volatile uint32_t core_last_task[tband_portNUMBER_OF_CORES] = {0};
 
 // ===== TRACE HOOKS ===========================================================
 
-// FIXME add version toggle
+#if (configUSE_TIMERS == 1)
+#define IMPL_TBAND_TIMER_TASK_STARTED()                                                            \
+  do {                                                                                             \
+    TaskHandle_t timer_svc = xTimerGetTimerDaemonTaskHandle();                                     \
+    uint32_t task_id = (uint32_t)uxTaskGetTaskNumber(timer_svc);                                   \
+    uint8_t buf[EVT_FREERTOS_TASK_IS_TIMER_TASK_MAXLEN];                                           \
+    size_t len = encode_freertos_task_is_timer_task(buf, task_id);                                 \
+    handle_trace_evt(buf, len, EVT_FREERTOS_TASK_IS_TIMER_TASK_IS_METADATA, ts);                   \
+  } while (0)
+#else /* (configUSE_TIMERS == 0) */
+#define IMPL_TBAND_TIMER_TASK_STARTED() ()
+#endif
+
+#if TBAND_FREERTOS_HAVE_VERSION(11, 2, 0)
+void impl_tband_freertos_scheduler_started_manual(void *xIdleTaskHandles) {
+  if (xIdleTaskHandles == NULL) return;
+  // Called inside vTaskStartScheduler so interrupts are disabled
+  uint64_t ts = tband_portTIMESTAMP();
+  for (size_t core_id = 0; core_id < configNUMBER_OF_CORES; core_id++) {
+    TaskHandle_t idle_task = ((TaskHandle_t *)xIdleTaskHandles)[core_id];
+    uint32_t task_id = (uint32_t)uxTaskGetTaskNumber(idle_task);
+    uint8_t buf[EVT_FREERTOS_TASK_IS_IDLE_TASK_MAXLEN];
+    size_t len = encode_freertos_task_is_idle_task(buf, task_id, core_id);
+    handle_trace_evt(buf, len, EVT_FREERTOS_TASK_IS_IDLE_TASK_IS_METADATA, ts);
+  }
+  IMPL_TBAND_TIMER_TASK_STARTED();
+}
+#else /* tband_configFREERTOS_VERSION < tband_configFREERTOS_VERSION_11_2_0 */
 #if (INCLUDE_xTaskGetIdleTaskHandle != 1)
 #error "INCLUDE_xTaskGetIdleTaskHandle is not enabled!"
 #endif /* INCLUDE_xTaskGetIdleTaskHandle */
 
-void impl_tband_freertos_scheduler_started_manual(void) {
+void impl_tband_freertos_scheduler_started_manual(void *xIdleTaskHandles) {
+  (void)xIdleTaskHandles;
   tband_portENTER_CRITICAL_FROM_ANY();
   uint64_t ts = tband_portTIMESTAMP();
 #if (tband_portNUMBER_OF_CORES > 1)
@@ -75,17 +116,10 @@ void impl_tband_freertos_scheduler_started_manual(void) {
     handle_trace_evt(buf, len, EVT_FREERTOS_TASK_IS_IDLE_TASK_IS_METADATA, ts);
   }
 #endif /* tband_portNUMBER_OF_CORES */
-#if (configUSE_TIMERS == 1)
-  {
-    TaskHandle_t timer_svc = xTimerGetTimerDaemonTaskHandle();
-    uint32_t task_id = (uint32_t)uxTaskGetTaskNumber(timer_svc);
-    uint8_t buf[EVT_FREERTOS_TASK_IS_TIMER_TASK_MAXLEN];
-    size_t len = encode_freertos_task_is_timer_task(buf, task_id);
-    handle_trace_evt(buf, len, EVT_FREERTOS_TASK_IS_TIMER_TASK_IS_METADATA, ts);
-  }
-#endif /* (configUSE_TIMERS == 1) */
+  IMPL_TBAND_TIMER_TASK_STARTED();
   tband_portEXIT_CRITICAL_FROM_ANY();
 }
+#endif
 
 #if (tband_configFREERTOS_TASK_TRACE_ENABLE == 1)
 void impl_tband_freertos_task_switched_in(uint32_t task_id) {
